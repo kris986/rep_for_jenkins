18277ea372c7a7223c7c64b515047dbaf7f4a803
kris986 <kris986@mail.ru>
2019-01-29 23:15:56 +0100

    try run web-server

--- run_web.py
+++ run_web.py
@@ -1,6 +1,9 @@
+import os
+
 from app import create_app
 from flask_heroku import Heroku
 
 
-app = create_app()
-# Heroku(app)
+app = create_app(os.getenv('FLASK_CONFIG') or 'dev')
+if __name__ == "__main__":
+    app.run()
