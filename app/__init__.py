05f0c0c627b808feed86c3cc1d8a9dd81b095b20
admin <jenkins@example.com>
2019-03-06 23:58:37 +0100

    changes in master for heroku

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 
-
 sentry_sdk.init(
     dsn="https://17882f9f9064472698e0f6b35210e1cf@sentry.io/1395740",
     integrations=[FlaskIntegration()]
@@ -10,6 +9,5 @@ sentry_sdk.init(
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 app.config.from_object('config')
 app.config['UPLOADED_PHOTOS_DEST'] = 'users_files'
-
 
 from app import views
