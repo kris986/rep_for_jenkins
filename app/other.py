dabeade670e55aae3cbde8c102d8210fdea516c6
admin <jenkins@example.com>
2019-03-10 23:47:49 +0100

    changes in master for heroku

--- app/other.py
+++ app/other.py
@@ -30,6 +30,6 @@ def list_of_sex_nms():
     return list_of_sex_nm
 
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# def allowed_file(filename):
+#     return '.' in filename and \
+#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
